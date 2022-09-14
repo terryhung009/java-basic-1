483b368c1ecd94ff4de710ed6b446a3f87c395ea
danny <khc741213@gmail.com>
2022-09-14 13:28:25 +0800

    1

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,7 +1,15 @@
 package com.codewithmosh;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+        int age = 30;
+        long viewCount = 3_123_456_789L;
+        float price = 10.99F;
+        Date now = new Date();
+//        now.getTime();
+        System.out.println(now);
     }
 }
