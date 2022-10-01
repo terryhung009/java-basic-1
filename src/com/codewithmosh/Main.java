7fd04a552c973d4eaff83fad873f52038b95b165
Denny <khc741213@gmail.com>
2022-10-02 00:29:20 +0800

    1

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,12 +4,21 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
+        /* 基本Hello World 輸出*/
 //        System.out.println("Hello World!");
-        int age = 30;
-        long viewCount = 3_123_456_789L;
-        float price = 10.99F;
-        Date now = new Date();
+//        int age = 30;
+//        long viewCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        Date now = new Date();
 //        now.getTime();
-        System.out.println(now);
+//        System.out.println(now);
+
+        /* String */
+//        String message = new String("Hello World!");
+        String message = "Hello World!" + "!!";
+        System.out.println(message);
+
+
+
     }
 }
