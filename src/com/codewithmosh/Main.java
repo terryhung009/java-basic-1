dd45f02452bd3682011c2cdcb125214e4fafa889
Denny <khc741213@gmail.com>
2022-10-02 10:28:53 +0800

    1

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,10 +1,12 @@
 package com.codewithmosh;
 
+import java.util.Arrays;
 import java.util.Date;
+
 
 public class Main {
     public static void main(String[] args){
-        /* 基本Hello World 輸出*/
+        /* 基本Hello World 輸出 與Primitive Types原始型別*/
 //        System.out.println("Hello World!");
 //        int age = 30;
 //        long viewCount = 3_123_456_789L;
@@ -13,11 +15,63 @@ public class Main {
 //        now.getTime();
 //        System.out.println(now);
 
-        /* String */
-//        String message = new String("Hello World!");
-        String message = "Hello World!" + "!!";
-        System.out.println(message);
+        /* String 物件操作 */
+//        String message = new String("Hello World");
+//        String message = "Hello World" + "!!";
+//        Boolean result = message.endsWith("!!");
+//        Boolean result = message.startsWith("!!");
+//        System.out.println(result);
 
+
+//        Integer number = message.length();
+//        System.out.println(number);
+//
+//        Integer number = message.indexOf("H");
+//        Integer number = message.indexOf("e");
+//        Integer number = message.indexOf("sky");
+//        System.out.println(number);
+
+//        String hello = message.replace("!","*");
+//        System.out.println(hello);
+
+//        String hello = message.toLowerCase();
+//        String hello = message.toUpperCase();
+//        System.out.println(hello);
+//        System.out.println(message);
+
+//        String message = "     Hello World" + "!!     ";
+//        System.out.println(message.trim());
+//        System.out.println(message);
+
+        /* escape sequence 轉義序列 跳脫字元 */
+//        String message = "Hello \"World\" ";
+//        System.out.println(message);
+
+        // c:\windows\work
+//        String message = "c:\\windows\\work";
+//        System.out.println(message);
+//        String message = "c:\nwindows\\work";
+//        System.out.println(message);
+//        String message = "c:\twindows\\work";
+//        System.out.println(message);
+
+        /* Arrays */
+//        int number = 1;
+//        int[] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//
+//        String numbers2 = Arrays.toString(numbers);
+//        System.out.println(numbers2);
+
+        
+
+
+
+
+        /* Arrays */
+        /* Arrays */
+        /* Arrays */
 
 
     }
